c1e91eff097676f7f2dca304d8c326f51386aded
Replit user <>
2023-09-06 10:05:02 +0000

    Write more detailed in4 in function

--- budget.py
+++ budget.py
@@ -1,34 +1,46 @@
 class Category:
+
   def __init__(self, name):
     self.name = name
     self.total = 0.0
+    self.ledger = []
 
-  def deposit(self, amount):
+  def deposit(self, amount, description):
     self.total += amount
+    self.ledger.append({"amount": amount, "description": description})
 
-  def withdraw(self, amount):
-    self.total -= amount
+  def withdraw(self, amount, description):
+    can_withdraw = self.check_funds(amount)
+    if can_withdraw:
+      self.total -= amount
+      self.ledger.append({"amount": -amount, "description": description})
+    return can_withdraw
 
   def get_balance(self):
     return self.total
-    
+
   def transfer(self, amount, instance):
+    canTransfer = self.check_funds(amount)
+    if canTransfer:
+      self.withdraw(amount, f"Transfer to {instance.name}")
+      instance.deposit(amount, f"Transfer to {self.name}")
+    return canTransfer
+
+  def check_funds(self, amount):
     if amount > self.total:
       return False
-
-    self.withdraw(amount)
-    instance.deposit(amount)
     return True
 
-  def check_funds(self):
-    pass
+
 food = Category("Food")
-entertainment =  Category("Entertainment")
-food.deposit(100)
+entertainment = Category("Entertainment")
+food.deposit(100, "something")
 food.transfer(25, entertainment)
+food.withdraw(20, "beans")
+print(food.ledger)
+print(entertainment.ledger)
 print(food.get_balance())
 print(entertainment.get_balance())
-
 
 
 def create_spend_chart(categories):
